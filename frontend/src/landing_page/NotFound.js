bfdfc9330f2037119b7664d8f56c3e58fa3a0c58
shashidhar078 <nagunurishashidhar@gmail.com>
2026-02-23 17:37:40 +0530

    implemented Not found component

---
+++ frontend/src/landing_page/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className="container p-5 mb-5 mt-5">
+      <div className="row text-center">
+         <h1 className="mt-5 fs-3">404 Not found</h1>
+         <p className="fs-6 mt-4 text-muted">We couldn’t find the page you were looking for.</p>
+        
+      </div>
+     
+    </div>
+  )
+}
+
+export default NotFound
