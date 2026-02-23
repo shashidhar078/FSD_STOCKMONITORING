e5befe905eec76512d0a7d7b79bb21abb331ea30
shashidhar078 <nagunurishashidhar@gmail.com>
2026-02-23 19:07:43 +0530

    implemented both hero and team sections in about component

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
 
-function Team() {
+function Hero() {
   return (
-    <div>Team</div>
-  )
+    <div className="container">
+      <div className="row p-3">
+        <h1 className="fs-3 text-center">People</h1>
+      </div>
+      <div
+        className="row p-3 text-muted"
+        style={{ lineHeight: "1.8", fontSize: "1em" }}
+      >
+        <div className="col-6 p-3 text-center">
+          <img
+            style={{ borderRadius: "100%", width: "60%" }}
+            src="media/images/nithinKamath.jpg"
+          />
+          <h5 className="mt-3">Nithin Kamath</h5>
+          <h6>Founder, CEO</h6>
+        </div>
+        <div className="col-6 p-3">
+          <p>
+            Nithin bootstrapped and founded Zerodha in 2010 to overcome the
+            hurdles he faced during his decade long stint as a trader. Today,
+            Zerodha has changed the landscape of the Indian broking industry.
+          </p>
+          <p>
+            He is a member of the SEBI Secondary Market Advisory Committee
+            (SMAC) and the Market Data Advisory Committee (MDAC).
+          </p>
+          <p>Playing basketball is his zen.</p>
+          <p>
+            Connect on{" "}
+            <a href="" style={{ textDecoration: "none" }}>
+              Homepage
+            </a>{" "}
+            /{" "}
+            <a href="" style={{ textDecoration: "none" }}>
+              TradingQnA
+            </a>{" "}
+            /{" "}
+            <a href="" style={{ textDecoration: "none" }}>
+              Twitter
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Team
+export default Hero;
