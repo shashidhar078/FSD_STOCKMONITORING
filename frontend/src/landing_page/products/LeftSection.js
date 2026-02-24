675c6b7776bd4fd2dfeaf60a103e02fa526ebd8e
shashidhar078 <nagunurishashidhar@gmail.com>
2026-02-24 19:38:26 +0530

    implemented left section component

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
-function LeftSection() {
+function LeftSection({
+  imageUrl,
+  productName,
+  productDescription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
   return (
-    <div>LeftSection</div>
-  )
+    <div className="container">
+      <div className="row">
+        <div className="col-6 p-3 mt-5">
+          <img src={imageUrl} />
+        </div>
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+          <div>
+            <a href={tryDemo} style={{ textDecoration: "none" }}>
+            Try demo
+          </a>{" "}
+          <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+          <a href={learnMore} style={{ textDecoration: "none" }}>
+            Learn More
+          </a>{" "}
+          <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+          </div>
+          <div className="mt-3">
+            <a href={googlePlay}>
+            <img src="media/images/googlePlayBadge.svg" />
+          </a>
+          <a href={appStore}>
+            <img src="media/images/appstoreBadge.svg" />
+          </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LeftSection
+export default LeftSection;
