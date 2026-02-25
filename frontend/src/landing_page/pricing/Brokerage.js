617daa0f88c16699a45dcad48864aafbcc29ca81
shashidhar078 <nagunurishashidhar@gmail.com>
2026-02-25 22:42:16 +0530

    implemented pricing-brokerage-component

--- frontend/src/landing_page/pricing/Brokerage.js
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 function Brokerage() {
   return (
-    <div>Brokerage</div>
-  )
+    <div className="container">
+      <div className="row p-5 border-top">
+        <div className="col-8">
+          <a href="" style={{ textDecoration: "none" }} className="text-center">
+            <h4>Brokerage calculator</h4>
+          </a>
+          <ul
+            style={{ textAlign: "left", lineHeight: "2.3", fontSize:"15px" }}
+            className="text-muted"
+          >
+            <li>
+              Calls & Trade and RMS auto-squaredoff:Additional charges of ₹59 +
+              GST per order{" "}
+            </li>
+            <li>Digital contract notes will be sent via e-mail</li>
+            <li>
+              Physical copies of contract notes,if required, shall be charged
+              ₹20 per contract note.Courier charges apply{" "}
+            </li>
+            <li>
+              if the account is in debit balance,any order placed will be
+              charged ₹40 per executed order instead of ₹20 per executed order
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <a href="" style={{ textDecoration: "none" }} className="text-center">
+            <h4>List of charges</h4>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Brokerage
+export default Brokerage;
