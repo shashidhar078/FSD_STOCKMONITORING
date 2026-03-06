df9ebc627d40199e2c0b9b05cf8926eb9305e3ec
shashidhar078 <nagunurishashidhar@gmail.com>
2026-03-06 15:18:37 +0530

    mongoose setup and mongodb connection is succesfull to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
+
+
 const express=require('express')
+const mongoose=require('mongoose')
+const uri=process.env.MONGO_URL;
+const port=process.env.PORT || 3001;
 
 const app=express();
 
-const port = 3001;
 
 app.listen(3001,()=>{
-    console.log(`app is listening on ${port}`)
+    console.log(`app is listening on ${port}`);
+    mongoose.connect(uri);
+    console.log("DB is connected");
 })
